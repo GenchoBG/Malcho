15cd8291ff04659e824dc6de5ac7613c852c5a93
Owliie <sladurcheto5232@abv.bg>
2020-05-14 10:15:22 +0300

    Added calculation of max y angle for the screen

--- setAngleByDeg.py
+++ setAngleByDeg.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+import RPi.GPIO as GPIO
+from time import sleep
+
 y_servo = 17
 x_servo = 27
-led = 22
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(y_servo, GPIO.OUT)
 GPIO.setup(x_servo, GPIO.OUT)
-GPIO.setup(led, GPIO.OUT)
 
 pwm_y = GPIO.PWM(y_servo, 50)
 pwm_y.start(2.5)
@@ -16,9 +17,7 @@ pwm_y.start(2.5)
 pwm_x = GPIO.PWM(x_servo, 50)
 pwm_x.start(2.5)
 
-def SetAngle(coords):
-  x, y = coords
-
+def SetAngle(x, y):
   x_duty = round(x / 18.0, 1) + 2.5
   y_duty = round(y / 18.0, 1) + 2.5
 
@@ -34,14 +33,10 @@ def SetAngle(coords):
   GPIO.output(x_servo, False)
   pwm_x.ChangeDutyCycle(0)
 
-  GPIO.output(led, GPIO.HIGH)
-
 def calculateAngle(x, y):
-  GPIO.output(led, GPIO.LOW)
-  max_angle_x = 45
-  max_angle_y = 28
+  max_angle = 45
 
-  return (x * max_angle_x, y * max_angle_y)
+  return (x * max_angle, y * max_angle)
 
 try:
   while True:
@@ -49,9 +44,8 @@ try:
     y = raw_input("y: ")
     x_fl = float(x)
     y_fl = float(y)
-    SetAngle(calculateAngle(x_fl, y_fl))
+    SetAngle(x_fl, y_fl)
 
 except KeyboardInterrupt:
   pwm_y.stop()
-  GPIO.output(led, GPIO.LOW)
   GPIO.cleanup()
